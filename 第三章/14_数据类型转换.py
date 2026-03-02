eb6285d20143dc3ea8da7d830927fb0850460188
mobei0726 <3027268257@qq.com>
2026-03-02 14:09:43 +0800

    Initial commit - Python 基础练习

---
+++ 第三章/14_数据类型转换.py
@@ -0,0 +1,15 @@
+# 使用str()，将其他数据类型转换为字符串类型
+# result1 = str(18)
+# result2 = str('18.53')
+# print(type(result1),result1)
+# print(type(result2),result2)
+
+# 使用float()，将其他数据类型转换为浮点类型
+# result1 = float(18)
+# result2 = float('18.53')
+# print(type(result1),result1)
+# print(type(result2),result2)
+
+#使用int(),将数据类型转换为整型
+result2 = int(18.53)
+print(type(result2),result2)
