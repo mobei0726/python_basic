b0ab8ebc87e14c7963d31615031a28932847e57d
mobei0726 <3027268257@qq.com>
2026-03-11 09:44:48 +0800

    python_study

---
+++ 第四章/09_嵌套循环.py
@@ -0,0 +1,17 @@
+day = 1
+for day in range(1,31):
+    print(f'第{day}天健身')
+    for group in range(1,4):
+        print(f'这是第{group}组仰卧起坐')
+    print(f'第{day}天的任务已完成！明天继续！\n')
+print(f'为期{day}天的健身计划完成！')
+
+day = 1
+while day <=30:
+    print(f'第{day}天健身')
+    group=1
+    while group <=3:
+        print(f'这是第{group}组仰卧起坐')
+        group+=1
+    day += 1
+print(f'为期{day-1}天的健身计划完成！')
