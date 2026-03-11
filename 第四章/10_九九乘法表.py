39643c92ea5c5f141b20ebb4cd2650ab3510cd6d
mobei0726 <3027268257@qq.com>
2026-03-11 10:13:56 +0800

    python_study

--- 第四章/10_九九乘法表.py
+++ 第四章/10_九九乘法表.py
@@ -6,14 +6,14 @@
 for row in range(1, 10):
     for item in range(1, row + 1):
         print(f'{item}*{row}={row * item}', end='\t')
-    print()# 每行结束后换行
+    print()  # 每行结束后换行
 
 # 使用while循环打印九九乘法表
 row = 1
 while row <= 9:
     item = 1
-    while item <= row:#项数循环只到row
+    while item <= row:  # 项数循环只到row
         print(f'{item}*{row}={row * item}', end='\t')
         item += 1
     row += 1
-    print()# 每行结束后换行
+    print()  # 每行结束后换行
